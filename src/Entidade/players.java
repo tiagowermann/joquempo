3086b682ddc4252fdcc91c1fec4945b066b4de9b
Tiago <tiagowermann90@gemail.com>
2022-02-07 21:24:40 -0300

    first commit

---
+++ src/Entidade/players.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidade;
+
+/**
+ *
+ * @author tiago
+ */
+public class players {
+
+    private String nomePlayer1;
+    private String lastnemPlayer1;
+    private String nomePlayer2;
+    private String lastnemPlayer2;
+    private String cpfPlayer1;
+    private String cpfPlayer2;
+
+    
+    
+    public String getNomePlayer1() {
+        return nomePlayer1;
+    }
+
+    public void setNomePlayer1(String nomePlayer1) {
+        this.nomePlayer1 = nomePlayer1;
+    }
+
+    public String getLastnemPlayer1() {
+        return lastnemPlayer1;
+    }
+
+    public void setLastnemPlayer1(String lastnemPlayer1) {
+        this.lastnemPlayer1 = lastnemPlayer1;
+    }
+
+    public String getNomePlayer2() {
+        return nomePlayer2;
+    }
+
+    public void setNomePlayer2(String nomePlayer2) {
+        this.nomePlayer2 = nomePlayer2;
+    }
+
+    public String getLastnemPlayer2() {
+        return lastnemPlayer2;
+    }
+
+    public void setLastnemPlayer2(String lastnemPlayer2) {
+        this.lastnemPlayer2 = lastnemPlayer2;
+    }
+
+    public String getCpfPlayer1() {
+        return cpfPlayer1;
+    }
+
+    public void setCpfPlayer1(String cpfPlayer1) {
+        this.cpfPlayer1 = cpfPlayer1;
+    }
+
+    public String getCpfPlayer2() {
+        return cpfPlayer2;
+    }
+
+    public void setCpfPlayer2(String cpfPlayer2) {
+        this.cpfPlayer2 = cpfPlayer2;
+    }
+
+   
+    
+    
+    
+    
+}
---
+++ src/Game/main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game;
+
+import Apresentacao.TelaInicialJogo;
+import java.util.Scanner;
+import Entidade.players;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author tiago
+ */
+public class main {
+
+    public static void main(String[] args) {
+//       players players = new players();
+//        
+//        TelaJogo tela = new TelaJogo(players);
+//        tela.exibir();
+//        
+//        System.exit(0);
+
+        new TelaInicialJogo().setVisible(true);
+
+    }
+
+}
